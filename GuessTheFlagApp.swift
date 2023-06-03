db67f0c5589a089b7eaf5cc0b871e4c50d240e69
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2023-06-03 13:45:13 +0200

    first commit

---
+++ GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sylvain Leguay on 01/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
